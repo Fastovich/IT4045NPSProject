a406ccc6952fa751511864563c7cb739aa77f1df
fastovich <lfastovich@gmail.com>
2023-09-26 17:35:38 -0400

    Added simple package for project

---
+++ src/main/java/com/npsproject/it4045npsproject/It4045NpsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.npsproject.it4045npsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It4045NpsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It4045NpsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/npsproject/it4045npsproject/It4045NpsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npsproject.it4045npsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It4045NpsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
