610416205ac310188430faa01cb7b2c9a9abac63
Kenny <keotero99@gmail.com>
2023-10-17 17:12:37 -0400

    Added service interface for parks

---
+++ src/main/java/com/npsproject/it4045npsproject/service/IParkService.java
@@ -0,0 +1,50 @@
+package com.npsproject.it4045npsproject.service;
+
+import com.npsproject.it4045npsproject.dto.Park;
+
+import java.util.List;
+
+/**
+ * Contains methods for business logic related to Parks
+ */
+public interface IParkService {
+    /**
+     * Save a park
+     * @param park - new park to be saved
+     * @return - newly saved park
+     */
+    Park save(Park park);
+
+    /**
+     * Delete a park
+     * @param park - park to be deleted
+     */
+    void delete(Park park);
+
+    /**
+     * Find a park with a given ID
+     * @param id - ID of a park
+     * @return - park
+     */
+    Park findParkById(int id);
+
+    /**
+     * Find a park in a specific location
+     * @param location - location
+     * @return - park
+     */
+    Park findParkByLocation(String location);
+
+    /**
+     * Add a favorite park
+     * @param park - park to be added as a favorite
+     */
+    void saveFavoritePark(Park park);
+
+    /**
+     * Return list of parks with the same activity
+     * @param activity - activity
+     * @return - list of parks
+     */
+    List<Park> findParksByActivity(String activity);
+}
