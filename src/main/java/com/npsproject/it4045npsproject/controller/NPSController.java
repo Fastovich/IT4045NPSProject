860c0611f98d9bc81ad0eb06f0a041126c50b933
wryther <rytherwy@mail.uc.edu>
2023-11-23 21:22:06 -0500

    Added jdoc for controller endpoints

--- src/main/java/com/npsproject/it4045npsproject/controller/NPSController.java
+++ src/main/java/com/npsproject/it4045npsproject/controller/NPSController.java
@@ -5,12 +5,13 @@ import com.npsproject.it4045npsproject.dto.ParkSearchResult;
 import com.npsproject.it4045npsproject.model.Park;
 import com.npsproject.it4045npsproject.service.ParkSearchService;
 import com.npsproject.it4045npsproject.service.ParkService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/parks")
 public class NPSController {
 
@@ -23,11 +24,20 @@ public class NPSController {
         this.parkSearchService = parkSearchService;
     }
 
+    /**
+     * Return a park based on the name inputted by the end user
+     * @param name string entered by end user in the interface
+     * @return park and park details
+     */
     @GetMapping("/park/{name}")
     public Park getParkByName(@PathVariable String name) {
         return parkService.getParkDetails(name);
     }
 
+    /**
+     * Return all parks
+     * @return list of all park objects
+     */
     @GetMapping("/park")
     @ResponseBody
     public List<Park> searchParks() {
